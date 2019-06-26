7ca7656ee6758fddcaf90801a0a174317fe9369d
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-06-26 03:11:37 +0300

    Implemented fasthttp routes for login route

--- routes/login_route.go
+++ routes/login_route.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/valyala/fasthttp"
+
 	"github.com/xSHAD0Wx/simple-api/services"
 )
 
@@ -33,6 +35,29 @@ func loginRoute(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, token)
 }
 
+func fastLoginRoute(ctx *fasthttp.RequestCtx) {
+	if !ctx.PostArgs().Has("username") || !ctx.PostArgs().Has("password") {
+		ctx.Error("Username and password are required", fasthttp.StatusBadRequest)
+		return
+	}
+
+	username := string(ctx.PostArgs().Peek("username"))
+	password := string(ctx.PostArgs().Peek("password"))
+
+	if username == "" || password == "" {
+		ctx.Error("Username and password are required", fasthttp.StatusBadRequest)
+		return
+	}
+
+	token, err := services.Services.AuthService.Authenticate(username, password)
+	if err != nil {
+		ctx.Error("Invalid username or password", fasthttp.StatusUnauthorized)
+		return
+	}
+
+	fmt.Fprint(ctx, token)
+}
+
 func registerRoute(res http.ResponseWriter, req *http.Request) {
 	err := req.ParseForm()
 	if err != nil {
@@ -56,7 +81,29 @@ func registerRoute(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-// GetLoginRouteGroup Gets the routes describtions of the login routes
+func fastRegisterRoute(ctx *fasthttp.RequestCtx) {
+	if !ctx.PostArgs().Has("username") || !ctx.PostArgs().Has("password") {
+		ctx.Error("Username and password are required", fasthttp.StatusBadRequest)
+		return
+	}
+
+	username := string(ctx.PostArgs().Peek("username"))
+	password := string(ctx.PostArgs().Peek("password"))
+	email := string(ctx.PostArgs().Peek("email"))
+
+	if username == "" || email == "" || password == "" {
+		ctx.Error("Empty values are not allowed", fasthttp.StatusBadRequest)
+		return
+	}
+
+	if err := services.Services.UsersRepo.AddUser(username, password, email, 2); err != nil {
+		ctx.Error("Could not register user", fasthttp.StatusInternalServerError)
+	} else {
+		ctx.Response.SetStatusCode(fasthttp.StatusCreated)
+	}
+}
+
+// GetLoginRouteGroup Gets the routes descriptions of the login routes
 func GetLoginRouteGroup() []HTTPRoute {
 	return []HTTPRoute{
 		HTTPRoute{
@@ -68,6 +115,22 @@ func GetLoginRouteGroup() []HTTPRoute {
 			Pattern: "/register",
 			Methods: []string{HTTPPost},
 			Handler: registerRoute,
+		},
+	}
+}
+
+// GetFastLoginRouteGroup Gets the fasthttp routes descriptions of the login routes
+func GetFastLoginRouteGroup() []FastHTTPRoute {
+	return []FastHTTPRoute{
+		FastHTTPRoute{
+			Pattern:  "/login",
+			UsesPost: true,
+			Handler:  fastLoginRoute,
+		},
+		FastHTTPRoute{
+			Pattern:  "/register",
+			UsesPost: true,
+			Handler:  fastRegisterRoute,
 		},
 	}
 }
