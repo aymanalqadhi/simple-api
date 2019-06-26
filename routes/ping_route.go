b736171090c20a5969390de5fdf3e9efdabfd125
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-06-26 03:25:18 +0300

    Removed old mux routes from the ping route group

--- routes/ping_route.go
+++ routes/ping_route.go
@@ -2,28 +2,12 @@ package routes
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/valyala/fasthttp"
 )
 
-func pingRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This site wont go down :D!\n")
-}
-
 func fastPingRoute(ctx *fasthttp.RequestCtx) {
 	fmt.Fprint(ctx, "This site wont go down :D!\n")
-}
-
-// GetPingRouteGroup Gets the routes describtions of the ping routes
-func GetPingRouteGroup() []HTTPRoute {
-	return []HTTPRoute{
-		HTTPRoute{
-			Pattern: "/ping",
-			Methods: []string{HTTPGet},
-			Handler: pingRoute,
-		},
-	}
 }
 
 // GetFastPingRouteGroup Gets the fasthttp routes describtions of the ping routes
