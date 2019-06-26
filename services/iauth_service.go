5e09fdd69d4a77653a7d81870796d7e013295c8c
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-06-26 03:17:11 +0300

    Added the fasthttp authorization middleware signature to the service interface

--- services/iauth_service.go
+++ services/iauth_service.go
@@ -1,10 +1,15 @@
 package services
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/valyala/fasthttp"
+)
 
 // IAuthService is an interface represening an authentication service
 type IAuthService interface {
 	Authenticate(string, string) (string, error)
 	ValidateToken(string) error
 	AuthorizedHandler(func(http.ResponseWriter, *http.Request)) http.Handler
+	AuthorizedFastHandler(fasthttp.RequestHandler) fasthttp.RequestHandler
 }
