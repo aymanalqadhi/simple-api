a02744b8acf881c9ccbef3aa5fc78901f2842d5c
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-06-26 03:11:47 +0300

    Implemented fasthttp routes for ping route

--- routes/ping_route.go
+++ routes/ping_route.go
@@ -3,10 +3,16 @@ package routes
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/valyala/fasthttp"
 )
 
 func pingRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "This site wont go down :D!\n")
+}
+
+func fastPingRoute(ctx *fasthttp.RequestCtx) {
+	fmt.Fprint(ctx, "This site wont go down :D!\n")
 }
 
 // GetPingRouteGroup Gets the routes describtions of the ping routes
@@ -16,6 +22,17 @@ func GetPingRouteGroup() []HTTPRoute {
 			Pattern: "/ping",
 			Methods: []string{HTTPGet},
 			Handler: pingRoute,
+		},
+	}
+}
+
+// GetFastPingRouteGroup Gets the fasthttp routes describtions of the ping routes
+func GetFastPingRouteGroup() []FastHTTPRoute {
+	return []FastHTTPRoute{
+		FastHTTPRoute{
+			Pattern: "/ping",
+			Handler: fastPingRoute,
+			UsesGet: true,
 		},
 	}
 }
