8ae706393f1766a7bc9b86af98052ce75de0e5b3
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-06-26 03:24:46 +0300

    Removed old mux routes from the login route group

--- routes/login_route.go
+++ routes/login_route.go
@@ -2,38 +2,10 @@ package routes
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/valyala/fasthttp"
-
 	"github.com/xSHAD0Wx/simple-api/services"
 )
-
-func loginRoute(res http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
-	if err != nil {
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	username := req.Form.Get("username")
-	password := req.Form.Get("password")
-
-	fmt.Println(username)
-	fmt.Println(password)
-	if username == "" || password == "" {
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	token, err := services.Services.AuthService.Authenticate(username, password)
-	if err != nil {
-		http.Error(res, "Invalid username or password", http.StatusUnauthorized)
-		return
-	}
-
-	fmt.Fprint(res, token)
-}
 
 func fastLoginRoute(ctx *fasthttp.RequestCtx) {
 	if !ctx.PostArgs().Has("username") || !ctx.PostArgs().Has("password") {
@@ -58,29 +30,6 @@ func fastLoginRoute(ctx *fasthttp.RequestCtx) {
 	fmt.Fprint(ctx, token)
 }
 
-func registerRoute(res http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
-	if err != nil {
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	username := req.Form.Get("username")
-	email := req.Form.Get("email")
-	password := req.Form.Get("password")
-
-	if username == "" || email == "" || password == "" {
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if err := services.Services.UsersRepo.AddUser(username, password, email, 2); err != nil {
-		http.Error(res, "Could not register user", http.StatusInternalServerError)
-	} else {
-		res.WriteHeader(http.StatusCreated)
-	}
-}
-
 func fastRegisterRoute(ctx *fasthttp.RequestCtx) {
 	if !ctx.PostArgs().Has("username") || !ctx.PostArgs().Has("password") {
 		ctx.Error("Username and password are required", fasthttp.StatusBadRequest)
@@ -100,22 +49,6 @@ func fastRegisterRoute(ctx *fasthttp.RequestCtx) {
 		ctx.Error("Could not register user", fasthttp.StatusInternalServerError)
 	} else {
 		ctx.Response.SetStatusCode(fasthttp.StatusCreated)
-	}
-}
-
-// GetLoginRouteGroup Gets the routes descriptions of the login routes
-func GetLoginRouteGroup() []HTTPRoute {
-	return []HTTPRoute{
-		HTTPRoute{
-			Pattern: "/login",
-			Methods: []string{HTTPPost},
-			Handler: loginRoute,
-		},
-		HTTPRoute{
-			Pattern: "/register",
-			Methods: []string{HTTPPost},
-			Handler: registerRoute,
-		},
 	}
 }
 
