8a89569c1016e94c340d19c224fffe987ec2635e
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-06-26 03:11:24 +0300

    Implemented fasthttp routes for client route

--- routes/clients_route.go
+++ routes/clients_route.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/valyala/fasthttp"
+
 	"github.com/gorilla/mux"
 
 	"github.com/xSHAD0Wx/simple-api/models"
@@ -24,7 +26,19 @@ func getClientsRoute(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(clients)
+}
 
+func fastGetClientsRoute(ctx *fasthttp.RequestCtx) {
+	clients, err := services.Services.ClientsRepo.GetAll()
+
+	if err != nil {
+		ctx.Error(http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Printf("Could not fetch clients for: %s", ctx.RemoteAddr())
+		return
+	}
+
+	ctx.Response.Header.Add("Content-Type", "application/json")
+	json.NewEncoder(ctx).Encode(clients)
 }
 
 func addClientRoute(w http.ResponseWriter, r *http.Request) {
@@ -49,6 +63,28 @@ func addClientRoute(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func fastAddClientRoute(ctx *fasthttp.RequestCtx) {
+	var client models.Client
+	err := json.Unmarshal(ctx.PostBody(), &client)
+
+	if err != nil {
+		ctx.Error(http.StatusText(fasthttp.StatusBadRequest), fasthttp.StatusBadRequest)
+		log.Printf("Bad request from %s", ctx.RemoteAddr())
+		return
+	}
+
+	id, err := services.Services.ClientsRepo.Add(client)
+
+	if err != nil {
+		ctx.Error(http.StatusText(fasthttp.StatusInternalServerError), fasthttp.StatusInternalServerError)
+		log.Printf("Could not add a client with name: %q, for: %s", client.Name, ctx.RemoteAddr())
+	} else {
+		log.Printf("A client was added, result id: %d", id)
+		ctx.Response.SetStatusCode(fasthttp.StatusCreated)
+		fmt.Fprint(ctx, id)
+	}
+}
+
 func deleteClientRoute(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseUint(mux.Vars(r)["id"], 10, 0)
 	if err != nil {
@@ -61,6 +97,24 @@ func deleteClientRoute(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		log.Printf("Could not delete client with id: %d, for: %s", id, r.RemoteAddr)
+	} else {
+		log.Printf("A client was deleted, result id: %d", id)
+	}
+}
+
+func fastDeleteClientRoute(ctx *fasthttp.RequestCtx) {
+
+	id, err := strconv.ParseUint(ctx.UserValue("id").(string), 10, 0)
+	if err != nil {
+		ctx.Error(http.StatusText(fasthttp.StatusBadRequest), fasthttp.StatusBadRequest)
+		log.Printf("Bad request from %s", ctx.RemoteAddr())
+		return
+	}
+
+	err = services.Services.ClientsRepo.Remove(uint(id))
+	if err != nil {
+		ctx.Error(http.StatusText(fasthttp.StatusInternalServerError), fasthttp.StatusInternalServerError)
+		log.Printf("Could not delete client with id: %d, for: %s", id, ctx.RemoteAddr())
 	} else {
 		log.Printf("A client was deleted, result id: %d", id)
 	}
@@ -86,6 +140,30 @@ func GetClientsRouteGroup() []HTTPRoute {
 			Methods:   []string{HTTPDelete},
 			Handler:   deleteClientRoute,
 			NeedsAuth: true,
+		},
+	}
+}
+
+// GetFastClientsRouteGroup Gets the fasthttp routes descriptions of the clients routes
+func GetFastClientsRouteGroup() []FastHTTPRoute {
+	return []FastHTTPRoute{
+		FastHTTPRoute{
+			Pattern:   "/clients",
+			Handler:   fastGetClientsRoute,
+			UsesGet:   true,
+			NeedsAuth: true,
+		},
+		FastHTTPRoute{
+			Pattern:   "/clients",
+			Handler:   fastAddClientRoute,
+			UsesPost:  true,
+			NeedsAuth: true,
+		},
+		FastHTTPRoute{
+			Pattern:    "/clients/:id",
+			Handler:    fastDeleteClientRoute,
+			UsesDelete: true,
+			NeedsAuth:  true,
 		},
 	}
 }
